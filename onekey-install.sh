e8e13f98a4eb2d3a83a9ef446fa4e3829c564705
StriveMoring <53570825+StriveMoring@users.noreply.github.com>
2022-03-20 00:53:17 +0800

    Update onekey-install.sh

--- onekey-install.sh
+++ onekey-install.sh
@@ -15,6 +15,27 @@ cur_dir=$(pwd)
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
 
+#remove v2ray
+set -x
+
+systemctl stop v2ray
+systemctl disable v2ray
+
+service v2ray stop
+update-rc.d -f v2ray remove
+
+rm -rf /usr/bin/v2ray /etc/init.d/v2ray /lib/systemd/system/v2ray.service
+
+set -
+
+echo "Logs and configurations are preserved, you can remove these manually"
+echo "logs directory: /var/log/v2ray"
+echo "configuration directory: /etc/v2ray"
+
+#remove acme.sh
+rm -rf /root/.acme.sh
+
+
 # check os
 if [[ -f /etc/redhat-release ]]; then
     release="centos"
